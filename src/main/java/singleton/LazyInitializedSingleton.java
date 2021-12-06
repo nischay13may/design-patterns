12a52b0e26e54ebaf5cef5d199e73cb447d2ebe1
Nischay Jain <nischay13may@gmail.com>
2021-12-06 16:55:35 +0530

    singleton pattern classes

---
+++ src/main/java/singleton/LazyInitializedSingleton.java
@@ -0,0 +1,18 @@
+package singleton;
+
+public class LazyInitializedSingleton {
+	
+	private static LazyInitializedSingleton instance;
+	
+	private LazyInitializedSingleton() {
+		
+	}
+	
+	public LazyInitializedSingleton getInstance() {
+		if(instance==null) {
+			instance = new LazyInitializedSingleton();
+		}
+		return instance;
+	}
+
+}
---
+++ src/main/java/singleton/StaticBlockSingleton.java
@@ -0,0 +1,23 @@
+package singleton;
+
+public class StaticBlockSingleton {
+	
+	private static final StaticBlockSingleton instance;
+	
+	static {
+		try {
+			instance = new StaticBlockSingleton();
+		}catch(Exception e) {
+			throw new RuntimeException("Exception occured in creating singleton instance");
+		}
+	}
+	
+	private StaticBlockSingleton() {
+		
+	}
+	
+	public static StaticBlockSingleton getInstance() {
+		return instance;
+	}
+
+}
